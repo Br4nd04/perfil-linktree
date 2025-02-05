a4d64dcc3c5dfc374411ea3fcb2898f03ef0e8e9
Brandon Ruiz <br972833124@gmail.com>
2025-02-05 18:59:44 -0500

    Linktree

---
+++ app.js
@@ -0,0 +1,23 @@
+const plantilla = document.getElementById("plantilla");
+
+plantilla.innerHTML = `
+  <div class="profile-container">
+    <img src="https://scontent.flim4-3.fna.fbcdn.net/v/t39.30808-6/461082837_8233951350034682_7513634044721239931_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeGQWbyVnsx6HopGNTiXlu3kl0lYan53pqCXSVhqfnemoOMaawKdJpcD1ziRMrXpQ6J_r2oO7hvuPW1VBwi3EwzK&_nc_ohc=OP_LmsZW0pgQ7kNvgFawGlS&_nc_zt=23&_nc_ht=scontent.flim4-3.fna&_nc_gid=AXu276CHehG1q8uKBq-p8rl&oh=00_AYCMxLlI3TQD1RSLWCRAp1locw8_rJS6SuLgMnProQhBDA&oe=67A9BB1D" alt="Profile Image" class="profile-image">
+    <div class="profile-name">Brandon Ruiz</div>
+    <div class="profile-description" > ★ Futuro desarrollador web ★ </div>
+    <a class="button" href="https://github.com/Br4nd04" target="_blank"> ➤ GitHub ◀ </a>
+    <a class="button" href="https://www.facebook.com/MarvelLatinoamerica/?brand_redir=6883542487" target="_blank"> ✓ Fan Marvel ✓ </a>
+    <a class="button" href="https://www.youtube.com/watch?v=MlwBZ2MSNtE&ab_channel=FISHER" target="_blank"> ♫ Música ♫ </a>
+    <div class="social-icons">
+      <a href="https://www.facebook.com/Br4nD.r" target="_blank">
+      <img src="https://c0.klipartz.com/pngpicture/136/802/gratis-png-facebook-iconos-de-la-computadora-logo-icono-de-facebook.png" alt="Facebook">
+      </a>
+      <a href="https://www.instagram.com/br4nd18/" target="_blank">
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLzFf7XeCguI-SWMhjUOx1kr8b6j9QppT2NQ&s" alt="Instagram">
+      </a>
+      <a href="https://open.spotify.com/user/31rcl4fqaahufrqmiwzzxqwilay4?si=0227db84e3b3470b" target="_blank">
+      <img src="https://cdn-icons-png.flaticon.com/512/154/154874.png" alt="Spotify">
+      </a>
+    </div>
+  </div>
+`;
